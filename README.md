68439840cbd282eed911dc2867c7f66c46fcc1a7
UrielCh <uriel.chemouni@gmail.com>
2023-05-30 18:17:05 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Circum Icons icons for deno / Preact
+
+**License** [MPL-2.0 license](https://github.com/Klarr-Agency/Circum-Icons/blob/main/LICENSE)
+
+**Project** [https://circumicons.com/](https://circumicons.com/)
+
+[See available icons here](https://react-icons.github.io/react-icons/icons?name=ci)
+
+## import_map.json
+
+For a transparent usage:
+
+```json
+{
+  "imports": {
+    "preact": "https://esm.sh/preact@10.15.1",
+    "preact/": "https://esm.sh/preact@10.15.1/",
+    "react-icons/ci": "https://deno.land/x/react_icons_ci@1.0.0//mod.ts",
+  }
+}
+```
+
+## Direct import sample
+
+`import { CiAirportSign1 } from "https://deno.land/x/react_icons_ci@1.0.0/mod.ts"`
+
+## import_map import sample
+
+`import { CiAirportSign1 } from "react-icons/ci"`
+
---
+++ mod.ts
@@ -0,0 +1,323 @@
+// Copyright 2022-2022 the Circum Icons authors. All rights reserved. MPL-2.0 license (https://github.com/Klarr-Agency/Circum-Icons/blob/main/LICENSE).
+/**
+ * # Circum Icons icons for deno / Preact
+ * 
+ * **License** [MPL-2.0 license](https://github.com/Klarr-Agency/Circum-Icons/blob/main/LICENSE)
+ * 
+ * **Project** [https://circumicons.com/](https://circumicons.com/)
+ * 
+ * [See available icons here](https://react-icons.github.io/react-icons/icons?name=ci)
+ * 
+ * ## import_map.json
+ * 
+ * For a transparent usage:
+ * 
+ * ```json
+ * {
+ *   "imports": {
+ *     "preact": "https://esm.sh/preact@10.15.1",
+ *     "preact/": "https://esm.sh/preact@10.15.1/",
+ *     "react-icons/ci": "https://deno.land/x/react_icons_ci@1.0.0//mod.ts",
+ *   }
+ * }
+ * ```
+ * 
+ * ## Direct import sample
+ * 
+ * `import { CiAirportSign1 } from "https://deno.land/x/react_icons_ci@1.0.0/mod.ts"`
+ * 
+ * ## import_map import sample
+ * 
+ * `import { CiAirportSign1 } from "react-icons/ci"`
+ * 
+ * @module
+ */
+
+export { CiAirportSign1 } from './ico/CiAirportSign1.ts';
+export { CiAlarmOff } from './ico/CiAlarmOff.ts';
+export { CiAlarmOn } from './ico/CiAlarmOn.ts';
+export { CiAlignBottom } from './ico/CiAlignBottom.ts';
+export { CiAlignCenterH } from './ico/CiAlignCenterH.ts';
+export { CiAlignCenterV } from './ico/CiAlignCenterV.ts';
+export { CiAlignLeft } from './ico/CiAlignLeft.ts';
+export { CiAlignRight } from './ico/CiAlignRight.ts';
+export { CiAlignTop } from './ico/CiAlignTop.ts';
+export { CiApple } from './ico/CiApple.ts';
+export { CiAt } from './ico/CiAt.ts';
+export { CiAvocado } from './ico/CiAvocado.ts';
+export { CiBacon } from './ico/CiBacon.ts';
+export { CiBadgeDollar } from './ico/CiBadgeDollar.ts';
+export { CiBag1 } from './ico/CiBag1.ts';
+export { CiBandage } from './ico/CiBandage.ts';
+export { CiBank } from './ico/CiBank.ts';
+export { CiBarcode } from './ico/CiBarcode.ts';
+export { CiBaseball } from './ico/CiBaseball.ts';
+export { CiBasketball } from './ico/CiBasketball.ts';
+export { CiBatteryCharging } from './ico/CiBatteryCharging.ts';
+export { CiBatteryEmpty } from './ico/CiBatteryEmpty.ts';
+export { CiBatteryFull } from './ico/CiBatteryFull.ts';
+export { CiBeaker1 } from './ico/CiBeaker1.ts';
+export { CiBeerMugFull } from './ico/CiBeerMugFull.ts';
+export { CiBellOff } from './ico/CiBellOff.ts';
+export { CiBellOn } from './ico/CiBellOn.ts';
+export { CiBezier } from './ico/CiBezier.ts';
+export { CiBitcoin } from './ico/CiBitcoin.ts';
+export { CiBluetooth } from './ico/CiBluetooth.ts';
+export { CiBookmarkCheck } from './ico/CiBookmarkCheck.ts';
+export { CiBookmarkMinus } from './ico/CiBookmarkMinus.ts';
+export { CiBookmarkPlus } from './ico/CiBookmarkPlus.ts';
+export { CiBookmarkRemove } from './ico/CiBookmarkRemove.ts';
+export { CiBookmark } from './ico/CiBookmark.ts';
+export { CiBowlNoodles } from './ico/CiBowlNoodles.ts';
+export { CiBoxList } from './ico/CiBoxList.ts';
+export { CiBoxes } from './ico/CiBoxes.ts';
+export { CiBrightnessDown } from './ico/CiBrightnessDown.ts';
+export { CiBrightnessUp } from './ico/CiBrightnessUp.ts';
+export { CiBullhorn } from './ico/CiBullhorn.ts';
+export { CiBurger } from './ico/CiBurger.ts';
+export { CiCalculator1 } from './ico/CiCalculator1.ts';
+export { CiCalculator2 } from './ico/CiCalculator2.ts';
+export { CiCalendarDate } from './ico/CiCalendarDate.ts';
+export { CiCalendar } from './ico/CiCalendar.ts';
+export { CiCamera } from './ico/CiCamera.ts';
+export { CiChat1 } from './ico/CiChat1.ts';
+export { CiChat2 } from './ico/CiChat2.ts';
+export { CiCircleAlert } from './ico/CiCircleAlert.ts';
+export { CiCircleCheck } from './ico/CiCircleCheck.ts';
+export { CiCircleChevDown } from './ico/CiCircleChevDown.ts';
+export { CiCircleChevLeft } from './ico/CiCircleChevLeft.ts';
+export { CiCircleChevRight } from './ico/CiCircleChevRight.ts';
+export { CiCircleChevUp } from './ico/CiCircleChevUp.ts';
+export { CiCircleInfo } from './ico/CiCircleInfo.ts';
+export { CiCircleList } from './ico/CiCircleList.ts';
+export { CiCircleMinus } from './ico/CiCircleMinus.ts';
+export { CiCircleMore } from './ico/CiCircleMore.ts';
+export { CiCirclePlus } from './ico/CiCirclePlus.ts';
+export { CiCircleQuestion } from './ico/CiCircleQuestion.ts';
+export { CiCircleRemove } from './ico/CiCircleRemove.ts';
+export { CiClock1 } from './ico/CiClock1.ts';
+export { CiClock2 } from './ico/CiClock2.ts';
+export { CiCloudDrizzle } from './ico/CiCloudDrizzle.ts';
+export { CiCloudMoon } from './ico/CiCloudMoon.ts';
+export { CiCloudOff } from './ico/CiCloudOff.ts';
+export { CiCloudOn } from './ico/CiCloudOn.ts';
+export { CiCloudRainbow } from './ico/CiCloudRainbow.ts';
+export { CiCloudSun } from './ico/CiCloudSun.ts';
+export { CiCloud } from './ico/CiCloud.ts';
+export { CiCoffeeBean } from './ico/CiCoffeeBean.ts';
+export { CiCoffeeCup } from './ico/CiCoffeeCup.ts';
+export { CiCoinInsert } from './ico/CiCoinInsert.ts';
+export { CiCoins1 } from './ico/CiCoins1.ts';
+export { CiCompass1 } from './ico/CiCompass1.ts';
+export { CiCreditCard1 } from './ico/CiCreditCard1.ts';
+export { CiCreditCard2 } from './ico/CiCreditCard2.ts';
+export { CiCreditCardOff } from './ico/CiCreditCardOff.ts';
+export { CiCrop } from './ico/CiCrop.ts';
+export { CiDark } from './ico/CiDark.ts';
+export { CiDatabase } from './ico/CiDatabase.ts';
+export { CiDeliveryTruck } from './ico/CiDeliveryTruck.ts';
+export { CiDesktopMouse1 } from './ico/CiDesktopMouse1.ts';
+export { CiDesktopMouse2 } from './ico/CiDesktopMouse2.ts';
+export { CiDesktop } from './ico/CiDesktop.ts';
+export { CiDiscount1 } from './ico/CiDiscount1.ts';
+export { CiDollar } from './ico/CiDollar.ts';
+export { CiDroplet } from './ico/CiDroplet.ts';
+export { CiDumbbell } from './ico/CiDumbbell.ts';
+export { CiEdit } from './ico/CiEdit.ts';
+export { CiEraser } from './ico/CiEraser.ts';
+export { CiExport } from './ico/CiExport.ts';
+export { CiFaceFrown } from './ico/CiFaceFrown.ts';
+export { CiFaceMeh } from './ico/CiFaceMeh.ts';
+export { CiFaceSmile } from './ico/CiFaceSmile.ts';
+export { CiFacebook } from './ico/CiFacebook.ts';
+export { CiFileOff } from './ico/CiFileOff.ts';
+export { CiFileOn } from './ico/CiFileOn.ts';
+export { CiFilter } from './ico/CiFilter.ts';
+export { CiFlag1 } from './ico/CiFlag1.ts';
+export { CiFloppyDisk } from './ico/CiFloppyDisk.ts';
+export { CiFolderOff } from './ico/CiFolderOff.ts';
+export { CiFolderOn } from './ico/CiFolderOn.ts';
+export { CiFootball } from './ico/CiFootball.ts';
+export { CiForkAndKnife } from './ico/CiForkAndKnife.ts';
+export { CiFries } from './ico/CiFries.ts';
+export { CiGift } from './ico/CiGift.ts';
+export { CiGlass } from './ico/CiGlass.ts';
+export { CiGlobe } from './ico/CiGlobe.ts';
+export { CiGps } from './ico/CiGps.ts';
+export { CiGrid2H } from './ico/CiGrid2H.ts';
+export { CiGrid2V } from './ico/CiGrid2V.ts';
+export { CiGrid31 } from './ico/CiGrid31.ts';
+export { CiGrid32 } from './ico/CiGrid32.ts';
+export { CiGrid41 } from './ico/CiGrid41.ts';
+export { CiGrid42 } from './ico/CiGrid42.ts';
+export { CiHardDrive } from './ico/CiHardDrive.ts';
+export { CiHashtag } from './ico/CiHashtag.ts';
+export { CiHeadphones } from './ico/CiHeadphones.ts';
+export { CiHeart } from './ico/CiHeart.ts';
+export { CiHome } from './ico/CiHome.ts';
+export { CiHospital1 } from './ico/CiHospital1.ts';
+export { CiHotdog } from './ico/CiHotdog.ts';
+export { CiIceCream } from './ico/CiIceCream.ts';
+export { CiImageOff } from './ico/CiImageOff.ts';
+export { CiImageOn } from './ico/CiImageOn.ts';
+export { CiImport } from './ico/CiImport.ts';
+export { CiInboxIn } from './ico/CiInboxIn.ts';
+export { CiInboxOut } from './ico/CiInboxOut.ts';
+export { CiIndent } from './ico/CiIndent.ts';
+export { CiInstagram } from './ico/CiInstagram.ts';
+export { CiKeyboard } from './ico/CiKeyboard.ts';
+export { CiLaptop } from './ico/CiLaptop.ts';
+export { CiLemon } from './ico/CiLemon.ts';
+export { CiLight } from './ico/CiLight.ts';
+export { CiLineHeight } from './ico/CiLineHeight.ts';
+export { CiLink } from './ico/CiLink.ts';
+export { CiLinkedin } from './ico/CiLinkedin.ts';
+export { CiLocationArrow1 } from './ico/CiLocationArrow1.ts';
+export { CiLocationOff } from './ico/CiLocationOff.ts';
+export { CiLocationOn } from './ico/CiLocationOn.ts';
+export { CiLock } from './ico/CiLock.ts';
+export { CiLogin } from './ico/CiLogin.ts';
+export { CiLogout } from './ico/CiLogout.ts';
+export { CiLollipop } from './ico/CiLollipop.ts';
+export { CiMail } from './ico/CiMail.ts';
+export { CiMapPin } from './ico/CiMapPin.ts';
+export { CiMap } from './ico/CiMap.ts';
+export { CiMaximize1 } from './ico/CiMaximize1.ts';
+export { CiMaximize2 } from './ico/CiMaximize2.ts';
+export { CiMedal } from './ico/CiMedal.ts';
+export { CiMedicalCase } from './ico/CiMedicalCase.ts';
+export { CiMedicalClipboard } from './ico/CiMedicalClipboard.ts';
+export { CiMedicalCross } from './ico/CiMedicalCross.ts';
+export { CiMedicalMask } from './ico/CiMedicalMask.ts';
+export { CiMemoPad } from './ico/CiMemoPad.ts';
+export { CiMenuBurger } from './ico/CiMenuBurger.ts';
+export { CiMenuFries } from './ico/CiMenuFries.ts';
+export { CiMenuKebab } from './ico/CiMenuKebab.ts';
+export { CiMicrochip } from './ico/CiMicrochip.ts';
+export { CiMicrophoneOff } from './ico/CiMicrophoneOff.ts';
+export { CiMicrophoneOn } from './ico/CiMicrophoneOn.ts';
+export { CiMinimize1 } from './ico/CiMinimize1.ts';
+export { CiMinimize2 } from './ico/CiMinimize2.ts';
+export { CiMobile1 } from './ico/CiMobile1.ts';
+export { CiMobile2 } from './ico/CiMobile2.ts';
+export { CiMobile3 } from './ico/CiMobile3.ts';
+export { CiMobile4 } from './ico/CiMobile4.ts';
+export { CiMoneyBill } from './ico/CiMoneyBill.ts';
+export { CiMoneyCheck1 } from './ico/CiMoneyCheck1.ts';
+export { CiMonitor } from './ico/CiMonitor.ts';
+export { CiMountain1 } from './ico/CiMountain1.ts';
+export { CiMug1 } from './ico/CiMug1.ts';
+export { CiMusicNote1 } from './ico/CiMusicNote1.ts';
+export { CiNoWaitingSign } from './ico/CiNoWaitingSign.ts';
+export { CiPalette } from './ico/CiPalette.ts';
+export { CiPaperplane } from './ico/CiPaperplane.ts';
+export { CiParking1 } from './ico/CiParking1.ts';
+export { CiPassport1 } from './ico/CiPassport1.ts';
+export { CiPause1 } from './ico/CiPause1.ts';
+export { CiPen } from './ico/CiPen.ts';
+export { CiPenpot } from './ico/CiPenpot.ts';
+export { CiPercent } from './ico/CiPercent.ts';
+export { CiPhone } from './ico/CiPhone.ts';
+export { CiPickerEmpty } from './ico/CiPickerEmpty.ts';
+export { CiPickerHalf } from './ico/CiPickerHalf.ts';
+export { CiPill } from './ico/CiPill.ts';
+export { CiPillsBottle1 } from './ico/CiPillsBottle1.ts';
+export { CiPizza } from './ico/CiPizza.ts';
+export { CiPlane } from './ico/CiPlane.ts';
+export { CiPlay1 } from './ico/CiPlay1.ts';
+export { CiPlug1 } from './ico/CiPlug1.ts';
+export { CiPower } from './ico/CiPower.ts';
+export { CiRainbow } from './ico/CiRainbow.ts';
+export { CiRead } from './ico/CiRead.ts';
+export { CiReceipt } from './ico/CiReceipt.ts';
+export { CiRedo } from './ico/CiRedo.ts';
+export { CiRepeat } from './ico/CiRepeat.ts';
+export { CiRollingSuitcase } from './ico/CiRollingSuitcase.ts';
+export { CiRoute } from './ico/CiRoute.ts';
+export { CiRouter } from './ico/CiRouter.ts';
+export { CiRuler } from './ico/CiRuler.ts';
+export { CiSatellite1 } from './ico/CiSatellite1.ts';
+export { CiSaveDown1 } from './ico/CiSaveDown1.ts';
+export { CiSaveDown2 } from './ico/CiSaveDown2.ts';
+export { CiSaveUp1 } from './ico/CiSaveUp1.ts';
+export { CiSaveUp2 } from './ico/CiSaveUp2.ts';
+export { CiSearch } from './ico/CiSearch.ts';
+export { CiServer } from './ico/CiServer.ts';
+export { CiSettings } from './ico/CiSettings.ts';
+export { CiShare1 } from './ico/CiShare1.ts';
+export { CiShare2 } from './ico/CiShare2.ts';
+export { CiShirt } from './ico/CiShirt.ts';
+export { CiShop } from './ico/CiShop.ts';
+export { CiShoppingBasket } from './ico/CiShoppingBasket.ts';
+export { CiShoppingCart } from './ico/CiShoppingCart.ts';
+export { CiShoppingTag } from './ico/CiShoppingTag.ts';
+export { CiShuffle } from './ico/CiShuffle.ts';
+export { CiSignpostDuo1 } from './ico/CiSignpostDuo1.ts';
+export { CiSignpostL1 } from './ico/CiSignpostL1.ts';
+export { CiSignpostR1 } from './ico/CiSignpostR1.ts';
+export { CiSliderHorizontal } from './ico/CiSliderHorizontal.ts';
+export { CiSliderVertical } from './ico/CiSliderVertical.ts';
+export { CiSpeaker } from './ico/CiSpeaker.ts';
+export { CiSquareAlert } from './ico/CiSquareAlert.ts';
+export { CiSquareCheck } from './ico/CiSquareCheck.ts';
+export { CiSquareChevDown } from './ico/CiSquareChevDown.ts';
+export { CiSquareChevLeft } from './ico/CiSquareChevLeft.ts';
+export { CiSquareChevRight } from './ico/CiSquareChevRight.ts';
+export { CiSquareChevUp } from './ico/CiSquareChevUp.ts';
+export { CiSquareInfo } from './ico/CiSquareInfo.ts';
+export { CiSquareMinus } from './ico/CiSquareMinus.ts';
+export { CiSquareMore } from './ico/CiSquareMore.ts';
+export { CiSquarePlus } from './ico/CiSquarePlus.ts';
+export { CiSquareQuestion } from './ico/CiSquareQuestion.ts';
+export { CiSquareRemove } from './ico/CiSquareRemove.ts';
+export { CiStar } from './ico/CiStar.ts';
+export { CiStethoscope } from './ico/CiStethoscope.ts';
+export { CiStickyNote } from './ico/CiStickyNote.ts';
+export { CiStop1 } from './ico/CiStop1.ts';
+export { CiStopSign1 } from './ico/CiStopSign1.ts';
+export { CiStopwatch } from './ico/CiStopwatch.ts';
+export { CiStreamOff } from './ico/CiStreamOff.ts';
+export { CiStreamOn } from './ico/CiStreamOn.ts';
+export { CiSun } from './ico/CiSun.ts';
+export { CiTablets1 } from './ico/CiTablets1.ts';
+export { CiTempHigh } from './ico/CiTempHigh.ts';
+export { CiTextAlignCenter } from './ico/CiTextAlignCenter.ts';
+export { CiTextAlignJustify } from './ico/CiTextAlignJustify.ts';
+export { CiTextAlignLeft } from './ico/CiTextAlignLeft.ts';
+export { CiTextAlignRight } from './ico/CiTextAlignRight.ts';
+export { CiText } from './ico/CiText.ts';
+export { CiTimer } from './ico/CiTimer.ts';
+export { CiTrash } from './ico/CiTrash.ts';
+export { CiTrophy } from './ico/CiTrophy.ts';
+export { CiTurnL1 } from './ico/CiTurnL1.ts';
+export { CiTurnR1 } from './ico/CiTurnR1.ts';
+export { CiTwitter } from './ico/CiTwitter.ts';
+export { CiUmbrella } from './ico/CiUmbrella.ts';
+export { CiUndo } from './ico/CiUndo.ts';
+export { CiUnlock } from './ico/CiUnlock.ts';
+export { CiUnread } from './ico/CiUnread.ts';
+export { CiUsb } from './ico/CiUsb.ts';
+export { CiUser } from './ico/CiUser.ts';
+export { CiVault } from './ico/CiVault.ts';
+export { CiVial } from './ico/CiVial.ts';
+export { CiVideoOff } from './ico/CiVideoOff.ts';
+export { CiVideoOn } from './ico/CiVideoOn.ts';
+export { CiViewBoard } from './ico/CiViewBoard.ts';
+export { CiViewColumn } from './ico/CiViewColumn.ts';
+export { CiViewList } from './ico/CiViewList.ts';
+export { CiViewTable } from './ico/CiViewTable.ts';
+export { CiViewTimeline } from './ico/CiViewTimeline.ts';
+export { CiVirus } from './ico/CiVirus.ts';
+export { CiVoicemail } from './ico/CiVoicemail.ts';
+export { CiVolumeHigh } from './ico/CiVolumeHigh.ts';
+export { CiVolumeMute } from './ico/CiVolumeMute.ts';
+export { CiVolume } from './ico/CiVolume.ts';
+export { CiWallet } from './ico/CiWallet.ts';
+export { CiWarning } from './ico/CiWarning.ts';
+export { CiWavePulse1 } from './ico/CiWavePulse1.ts';
+export { CiWheat } from './ico/CiWheat.ts';
+export { CiWifiOff } from './ico/CiWifiOff.ts';
+export { CiWifiOn } from './ico/CiWifiOn.ts';
+export { CiYoutube } from './ico/CiYoutube.ts';
+export { CiZoomIn } from './ico/CiZoomIn.ts';
+export { CiZoomOut } from './ico/CiZoomOut.ts';
