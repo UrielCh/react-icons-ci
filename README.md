748e270c9d15e0e99dd6583a4803de52984a9e88
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:08 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/ci)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/ci
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/ci
 bunx jsr add @preact-icons/ci
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Circum Icons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/ci)](https://jsr.io/@preact-icons/ci)
-
-**License** [MPL-2.0 license](https://github.com/Klarr-Agency/Circum-Icons/blob/main/LICENSE)
-
-**Project** [https://circumicons.com/](https://circumicons.com/)
-
-[See available icons here](https://react-icons.deno.dev/ci)
-
-## install packahe
-
-```bash
-deno add @preact-icons/ci
-dnpx jsr add @preact-icons/ci
-pnpm dlx jsr add @preact-icons/ci
-bunx jsr add @preact-icons/ci
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import CiAirportSign1 from "react-icons/ci/CiAirportSign1.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/ci)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/ci
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/ci
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Circum Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/ci)](https://jsr.io/@preact-icons/ci)
- * 
- * **License** [MPL-2.0 license](https://github.com/Klarr-Agency/Circum-Icons/blob/main/LICENSE)
- * 
- * **Project** [https://circumicons.com/](https://circumicons.com/)
- * 
- * [See available icons here](https://react-icons.deno.dev/ci)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/ci
- * dnpx jsr add @preact-icons/ci
- * pnpm dlx jsr add @preact-icons/ci
- * bunx jsr add @preact-icons/ci
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import CiAirportSign1 from "react-icons/ci/CiAirportSign1.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
